9ad2a6643e025c52c45c43f3bf1eb4fdc7612d90
Deyber Castillo <53123654+11dcastillo@users.noreply.github.com>
2021-02-12 00:29:25 -0600

    Agregar archivos de proyecto.

---
+++ Tarea1P3_AbrirlExcel/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using tareaxls.clases;
+
+namespace Tarea1P3_AbrirlExcel
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var estructura = new clsEstructura().cargaDatosXLS();
+            int pausa;
+            pausa = 0;
+
+
+        }
+    }
+}
---
+++ Tarea1P3_AbrirlExcel/clases/clsEstructura.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using Excel = Microsoft.Office.Interop.Excel;
+
+namespace tareaxls.clases
+{
+    class clsEstructura
+    {
+        public string nombre { get; set; }
+        public string direccion { get; set; }
+
+        public List<clsEstructura> cargaDatosXLS()
+        {
+            Excel.Application xlApp;
+            Excel.Workbook xlWorkBook;
+            Excel.Worksheet xlWorkSheet;
+            Excel.Range range;
+
+            int rCnt;
+            int rw = 0;
+            int cl = 0;
+
+            xlApp = new Excel.Application();
+            xlWorkBook = xlApp.Workbooks.Open(@"C:\Users\casti\Desktop\tarea1 - copia.xlsx");
+            xlWorkSheet = (Excel.Worksheet)xlWorkBook.Worksheets.get_Item(1);
+
+            range = xlWorkSheet.UsedRange;
+            rw = range.Rows.Count;
+            cl = range.Columns.Count;
+
+            List<clsEstructura> todos = new List<clsEstructura>();
+            clsEstructura individual = new clsEstructura();
+
+            for (rCnt = 1; rCnt <= rw; rCnt++)
+            {
+
+                individual.nombre = (string)(range.Cells[rCnt, 1] as Excel.Range).Value2;
+                individual.direccion = (string)(range.Cells[rCnt, 2] as Excel.Range).Value2;
+
+                todos.Add(individual);
+                individual = new clsEstructura();
+            }
+
+            xlWorkBook.Close(true, null, null);
+            xlApp.Quit();
+
+            Marshal.ReleaseComObject(xlWorkSheet);
+            Marshal.ReleaseComObject(xlWorkBook);
+            Marshal.ReleaseComObject(xlApp);
+
+            return todos;
+
+        }
+
+
+
+    }
+
+
+
+}
+
